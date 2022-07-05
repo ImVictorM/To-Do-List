ce2b7bb73d5028211b52d777baef728abec207f6
Victor Mendes <victorutdsim7@gmail.com>
2022-07-05 13:15:54 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+function onClickChangeBgColor(element) {
+  element.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'grey';
+  });
+}
+
+
 const addButton = document.getElementById('criar-tarefa');
 addButton.addEventListener('click', () => {
   const inputText = document.getElementById('texto-tarefa').value;
   document.getElementById('texto-tarefa').value = ''; // reset
   const listItem = document.createElement('li');
   listItem.innerText = inputText;
+  onClickChangeBgColor(listItem);
   const list = document.getElementById('lista-tarefas');
   list.appendChild(listItem);
 });
+
+
